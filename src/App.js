9e721704415d5234505b3cfd74f3dc81c0753fb6
pkihiu <p.kihiu@alustudent.com>
2024-10-17 14:35:26 +0300

    updated the files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from "./pages/Landing";
 import Features from "./pages/Features";
 import Resources from "./components/Resources";
+import Pricing from './components/Pricing';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/features" element={<Features />} />
           <Route path="/resources" element={<Resources />} />
+          <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </Router>
   );
